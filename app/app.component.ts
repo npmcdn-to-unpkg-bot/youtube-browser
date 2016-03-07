c638d1f8055e9b2813f6de8664d194980807931c
barron <barron@conquistador.hagermanenator.net>
2016-03-06 23:50:10 -0600

    Simple Angular 2 starting point with successful connection and search with YouTube API (console logging only)

---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component} from 'angular2/core';
+import {YouTubeVideo} from './youtubevideo';
+import {YouTubeSearchComponent} from './youtube-search.component';
+
+declare var gapi : any;
+
+@Component({
+  selector: 'youtube-browser',
+  template:`
+    <h1>{{title}}</h1>
+    <youtube-search></youtube-search>
+  `,
+  directives: [YouTubeSearchComponent]
+})
+
+export class AppComponent {
+  public title = 'YouTube Browser';
+  
+  //Google Browser API Key
+  API_KEY = 'AIzaSyBIN5pEfYIU-IULSw121ojZ9xxf8RShFBA';
+
+  ngOnInit() { this.google_api_init(); }
+  
+  //Initialize Google API
+  google_api_init() {
+    gapi.client.setApiKey(this.API_KEY);
+    
+    //load the YouTube API; when ready, execute YouTubeAPILoaded()
+    gapi.client.load('youtube', 'v3', function() { YouTubeSearchComponent.YouTubeAPILoaded(); });
+  }
+}
---
+++ app/youtubevideo.ts
@@ -0,0 +1,4 @@
+export interface YouTubeVideo {
+  id: number,
+  snippet: Object
+}
